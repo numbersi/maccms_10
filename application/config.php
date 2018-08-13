1fae5d2873d4764550a65cd753e24fce538dc588
javaxx <136222866@qq.com>
2018-08-13 15:53:11 +0800

    更改模版js css imge 到七牛路径

--- application/config.php
+++ application/config.php
@@ -148,6 +148,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [
         '__STATIC__' => 'http://kuainiaobucket.numbersi.cn/www/maccms101/static',
+        '__TEMPLATE__' => 'http://kuainiaobucket.numbersi.cn/www/maccms101',
 //        '__ROOT__' => '/',
     ],
     // 默认跳转页面对应的模板文件
